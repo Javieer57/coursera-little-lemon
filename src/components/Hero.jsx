abf73aa44263534269836b4a7585fcc00d2926c7
Javier Eufracio <jangel@ia.com.mx>
2023-11-24 15:34:43 -0600

    add Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import "./Hero.css";
+
+export const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-left">
+        <h1 className="hero-heading">Little Lemon</h1>
+
+        <p className="hero-text">Chicago</p>
+
+        <p className="hero-subtext">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo,
+          obcaecati. Ipsa fuga quaerat minima quis at exercitationem nulla
+        </p>
+
+        <button className="btn btn-primary btn-icon">
+          <img src="./icons/menu.svg" alt="" />
+          <span>Reserve a table</span>
+        </button>
+      </div>
+
+      <div className="hero-right">
+        <img src="./img/restaurant-food.jpg" />
+      </div>
+    </section>
+  );
+};
