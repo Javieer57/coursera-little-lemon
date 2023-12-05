36272f5d232558366ffb8eff65bdff14d4e80dbf
Javier Eufracio <jangel@ia.com.mx>
2023-12-05 09:54:54 -0600

    add Testimonials section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 import Specials from "./components/Specials";
+import { Testimonials } from "./components/Testimonials";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Hero />
         <Specials />
+        <Testimonials />
       </main>
     </div>
   );
